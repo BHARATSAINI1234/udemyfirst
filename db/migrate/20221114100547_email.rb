eb1085d1c71e3d427bb555246a04aca0e7cf80d0
BHARATSAINI1234 <sainibharat588@email.com>
2022-11-15 11:12:50 +0530

    Add user table and user model

--- db/migrate/20221114100547_email.rb
+++
@@ -1,5 +0,0 @@
-class AddUserMail < ActiveRecord::Migration[6.0]
-  def change
-    add_column: email, email:, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2022_11_14_081741) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
