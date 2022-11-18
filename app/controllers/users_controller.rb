bf66dfcb9a777cac93620aed510fa6dc927c84cc
BHARATSAINI1234 <sainibharat588@email.com>
2022-11-18 14:45:17 +0530

    add alert message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -38,6 +38,15 @@ class UsersController < ApplicationController
     end
   end
 
+
+  def destroy
+    @user.destroy
+    session[:user_id] = nil if @user == current_user
+    flash[:notice] = "Account and all associated articles successfully deleted"
+    redirect_to articles_path
+  end
+
+
   private
     def user_params
       params.require(:user).permit(:username, :email, :password)
@@ -47,4 +56,10 @@ class UsersController < ApplicationController
       @user = User.find(params[:id])
     end
 
+    def require_same_user
+      if current_user != @user && !current_user.admin?
+        flash[:alert] = "You can only edit or delete your own account"
+        redirect_to @user
+      end
+    end
 end
