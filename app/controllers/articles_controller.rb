8a5bd16a511a99d5f9b0eca1f355a1c7e76cdb50
BHARATSAINI1234 <sainibharat588@email.com>
2022-11-18 11:52:08 +0530

    login

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,7 +5,7 @@ class ArticlesController < ApplicationController
   end
 
   def index
-    @article = Article.all
+    @articles = Article.paginate(page: params[:page], per_page: 5)
   end
 
   def new
@@ -17,7 +17,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-    @article.user = User.first
+     @article.user = current_user
     if @article.save
       flash[:notice] = "Articles was created sucessfully."
       redirect_to @article
